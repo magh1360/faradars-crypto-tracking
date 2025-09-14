f010d6e3462dbd61b65ce9872fdaa8d499c83050
magh1360 <ma.ghorbani@gmail.com>
2025-09-14 14:45:10 +0330

    contact us and about us implemented

--- src/pages/About-Us.page.jsx
+++ src/pages/About-Us.page.jsx
@@ -1,13 +1,24 @@
 import React from "react";
-const FAQPage = () =>{
+import aboutImg from "../assets//images/about-us.png";
+const FAQPage = () => {
 
-    return(
-        <div>
-          <h5>About-Us</h5>
-          <h2>About-Us</h2>
-          <h2>About-Us</h2>
-          <h2>About-Us</h2>
+  return (
+    <div className="container">
+      <div className="row ">
+        <div className="col-12 bg-info p-3 rounded ">
+          <h3>About Us</h3>
         </div>
-    );
+      </div>
+      <div className="row   align-items-center ">
+        <div className="col-md-7">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dolores animi, aliquam praesentium harum ducimus deleniti aspernatur reiciendis non quod consequatur repellendus saepe voluptate aperiam architecto dolorum nobis vel quaerat quo hic rem, eaque velit tenetur minima! Voluptatem sequi incidunt modi ullam ducimus voluptates iure et blanditiis voluptate dolore tempora quaerat maiores praesentium libero, eos, dolor enim! Nam, aliquid. Tempore omnis nemo eligendi maiores nulla repudiandae quia odit asperiores illum, vel cupiditate tempora excepturi aliquid! Officia doloribus facere voluptatum eaque mollitia consequatur quas error! Quas, asperiores. Illum, rem amet doloribus, voluptatum laborum in quod dolorem vel labore dicta, a optio.
+        </div>
+        <div className="col-md-5 text-center">
+          <img src={aboutImg} alt="about us" className="img-fluid about-img" />
+        </div>
+      </div>
+
+    </div>
+  );
 };
 export default FAQPage;
--- src/pages/Contact-Us.page.jsx
+++ src/pages/Contact-Us.page.jsx
@@ -1,13 +1,41 @@
 import React from "react";
-const FAQPage = () =>{
+import contactImg from "../assets/images/contact-us.png"
+const FAQPage = () => {
 
-    return(
-        <div>
-          <h2>Contact-Us</h2>
-          <h2>Contact-Us</h2>
-          <h2>Contact-Us</h2>
-          <h2>Contact-Us</h2>
+  return (
+    <div className="container">
+      <div className="row ">
+        <div className="col-12 bg-info p-3 rounded ">
+          <h3>Contact Us</h3>
         </div>
-    );
+      </div>
+      <div className="row   align-items-center mt-3">
+        <div className="col-md-7 ">
+          <div>
+            <h3 className="text-primary">How to contact us</h3>
+            <h5>0912 555 5555</h5>
+            <h5>021 222222222</h5>
+            <h5>test@faradars.com</h5>
+          </div>
+          <div className="mt-3">
+            <div className="mb-3">
+              <label className="form-label">Your Name</label>
+              <input type="text" className="form-control" placeholder="Please type your name" />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Your Message</label>
+              <input type="text" className="form-control" placeholder="Please type your message" />
+            </div>
+            <button className="btn btn-outline-primary ms-3" >Send</button>
+          </div>
+        </div>
+
+        <div className="col-md-5 text-center">
+          <img src={contactImg} alt="about us" className="img-fluid about-img" />
+        </div>
+      </div>
+
+    </div>
+  );
 };
 export default FAQPage;
